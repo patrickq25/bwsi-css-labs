57a8ea577d787105ec0caef703d9e2a01aa0da8d
patrickq25 <patrickq2025@gmail.com>
2026-06-08 21:39:29 -0400

    Added Print Introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Patrick Qiu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello! My name is Patrick and I am from Maryland. A fun fact about me is that I am on an FTC robotics team, and we actually use GitHub for our coding. I am excited to learn with the course here!")
 
 if __name__ == "__main__":
     main()
